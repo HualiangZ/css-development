7d47c06a173b76a81f369272b91942b299be471f
HualiangZ <zhualiang88@gmail.com>
2024-08-29 22:11:44 +0100

    small text changes

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -33,32 +33,32 @@ function FirstToggleBtn() {
             <div ref={constainerRef} className='ToggleGroup'>
                 <div className='CustomInput'>
                     <input type="radio" className="Button1" id='Button1' name='action1' value="Button1" />
-                    <label for='Button1'>Button 1</label>
+                    <label for='Button1'>Untitled 1</label>
                 </div>
 
                 <div className='CustomInput'>
                     <input type="radio" className="Button2" id='Button2' name='action1' value="Button2" />
-                    <label for='Button2'>Button 2</label>
+                    <label for='Button2'>Untitled 2</label>
                 </div>
 
                 <div className='CustomInput'>
                     <input type="radio" className="Button3" id='Button3' name='action1' value="Button3" />
-                    <label for='Button3'>Button 3</label>
+                    <label for='Button3'>Untitled 3</label>
                 </div>
 
                 <div className='CustomInput'>
                     <input type="radio" className="Button4" id='Button4' name='action1' value="Button4" />
-                    <label for='Button4'>Button 4</label>
+                    <label for='Button4'>Untitled 4</label>
                 </div>
 
                 <div className='CustomInput'>
                     <input type="radio" className="Button5" id='Button5' name='action1' value="Button5" />
-                    <label for='Button5'>Button 5</label>
+                    <label for='Button5'>Untitled 5</label>
                 </div>
 
                 <div className='CustomInput'>
                     <input type="radio" className="Button6" id='Button6' name='action1' value="Button6" />
-                    <label for='Button6'>Button 6</label>
+                    <label for='Button6'>Untitled 6</label>
                 </div>
             </div>
             <div className='ArrowBtnRight' onClick={() => Scroll(200)}>
@@ -98,32 +98,32 @@ function SecondToggleBtn() {
             <div ref={constainerRef} className="ToggleGroup2">
                 <div className='CustomInput2'>
                     <input type="radio" className="Button7" id='Button7' name='action' value="Button1" />
-                    <label for='Button7'>Button 1</label>
+                    <label for='Button7'>Untitled 1</label>
                 </div>
 
                 <div className='CustomInput2'>
                     <input type="radio" className="Button8" id='Button8' name='action' value="Button2" />
-                    <label for='Button8'>Button 2</label>
+                    <label for='Button8'>Untitled 2</label>
                 </div>
 
                 <div className='CustomInput2'>
                     <input type="radio" className="BuButton9tton3" id='Button9' name='action' value="Button3" />
-                    <label for='Button9'>Button 3</label>
+                    <label for='Button9'>Untitled 3</label>
                 </div>
 
                 <div className='CustomInput2'>
                     <input type="radio" className="Button10" id='Button10' name='action' value="Button4" />
-                    <label for='Button10'>Button 4</label>
+                    <label for='Button10'>Untitled 4</label>
                 </div>
 
                 <div className='CustomInput2'>
                     <input type="radio" className="Button11" id='Button11' name='action' value="Button5" />
-                    <label for='Button11'>Button 5</label>
+                    <label for='Button11'>Untitled 5</label>
                 </div>
 
                 <div className='CustomInput2'>
                     <input type="radio" className="Button12" id='Button12' name='action' value="Button6" />
-                    <label for='Button12'>Button 6</label>
+                    <label for='Button12'>Untitled 6</label>
                 </div>
             </div>
             <div className='ArrowBtnRight' onClick={() => Scroll(200)}>
@@ -219,7 +219,7 @@ export default function Page2() {
                     {FirstToggleBtn()}
                     <p className='SubTitle'>Some Text here</p>
                     {SecondToggleBtn()}
-                    <p className='SubTitle'>Some other Text here</p>
+                    <p className='SubTitle2'>Some other Text here</p>
                     <div className='Folders'>
                         {Folders()}
                     </div>
